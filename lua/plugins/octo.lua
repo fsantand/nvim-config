6552c3850e61fe0bbe22e6f306c585ef2f513dd0
Federico Santander <federico.santander@bci.cl>
2024-10-22 18:25:23 -0300

    feat(octo): add pendings prs keymap

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -9,4 +9,11 @@ return {
     require"octo".setup()
   end,
   cmd = "Octo",
+  keys = {
+    {
+      "<leader>op",
+      "<cmd>Octo search is:open is:pr review-requested:fsantand -author:app/dependabot NOT Dockerfile<cr>",
+      desc="Octo: View PRs review"
+    }
+  }
 }
