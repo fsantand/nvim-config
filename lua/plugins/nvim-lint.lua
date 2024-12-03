a0be420a60fcc7dbed13522184ed0e35fbc365b2
Federico Santander <federico.santander@bci.cl>
2024-12-03 16:42:07 -0300

    chore(weas): aksdjakdj

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -5,6 +5,7 @@ return {
     local lint = require("lint")
     lint.linters_by_ft = {
       javascript = {'eslint_d',},
+      json = {'jsonlint',},
     }
 
     vim.api.nvim_create_autocmd({ "BufWritePost" }, {
