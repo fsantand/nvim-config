b4ef1e2f704dfde2ce5fc182c5d02725223ca40e
Federico Santander <fsantander@trytoku.com>
2026-05-13 10:35:36 -0400

    config: change colorscheme transaparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,8 +3,6 @@ vim.g.sonokai_enable_italic = true
 require("flexoki").setup({})
 require("gruvbox").setup({})
 require("vague").setup({})
-require("dracula").setup({
-  transparent_bg = true,
-})
+require("dracula").setup({})
 
 vim.cmd.colorscheme("dracula")
